3a95b5566101c7e572a1efb5918f70c2675f824c
vamsi2018 <svkris@TheSvKris.(none)>
2013-06-24 22:09:25 +0530

    insertion sort implemented

--- sorting/insertionSort.c
+++ sorting/insertionSort.c
@@ -1,5 +1,42 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
+	int *array,i=0,N,j=0,temp=0;
+	printf("Enter array size : ");
+	scanf("%d",&N);
+
+	array=malloc(sizeof(int)*N);
+	if(array==NULL){
+		printf("No memory!!!");
+		return 0;
+	}
+	int exit=0;
+	for(i=0;i<N;i++){
+		printf("\nEnter number ");
+		scanf("%d",&array[i]);
+	}
+
+	for(i=1;i<N;i++){
+		temp = array[i];
+		exit=0;
+		for(j=i-1;j>=0&&exit!=1;j--){
+			if(temp<array[j]){
+				array[j+1]=array[j];
+				array[j]=temp;
+			}
+			else{
+//				array[j]=temp;
+				exit=1;
+			}
+		}
+//		array[++j]=temp;		
+	}
+
+	printf("Insertion sort performed\n");
+
+	for(i=0;i<N;i++){
+		printf("\n%d -> %d\n",i,array[i]);
+	}
 
 	return 0;
 }
